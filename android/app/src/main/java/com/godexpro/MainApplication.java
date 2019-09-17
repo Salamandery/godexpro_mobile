ccbcc661765b17f5e5ea3ee120095a4b9b02f600
salamandery <sp.ront@hotmail.com>
2019-09-17 20:19:25 -0300

    codepush

--- android/app/src/main/java/com/godexpro/MainApplication.java
+++ android/app/src/main/java/com/godexpro/MainApplication.java
@@ -29,13 +29,9 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      @SuppressWarnings("UnnecessaryLocalVariable")
-      List<ReactPackage> packages = new PackageList(this).getPackages();
-      // Packages that cannot be autolinked yet can be added manually here, for example:
-      // packages.add(new MyReactNativePackage());
       return Arrays.<ReactPackage>asList(
-                new MainReactPackage(),
-                new CodePush(BuildConfig.CODEPUSH_KEY, MainApplication.this, BuildConfig.DEBUG)
+          new MainReactPackage(),
+          new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG)
       );
     }
 
