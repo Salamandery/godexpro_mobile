d47809a740833a1f838c3c3443b1fe4c1eb37e67
salamandery <sp.ront@hotmail.com>
2019-09-19 20:42:17 -0300

    components adsense

---
+++ src/components/AdSense/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+    View,
+    StyleSheet
+} from 'react-native';
+import {
+    AdMobBanner,
+    AdMobInterstitial,
+    PublisherBanner,
+    AdMobRewarded,
+} from 'react-native-admob';
+
+import { normalize } from '../StringTrataments';
+
+const AdSense = () => {
+    return (
+        <View style={styles.containerAD}>
+            <AdMobBanner
+                adSize="smartBannerPortrait"
+                adUnitID="ca-app-pub-5356914343536529/6894497090"
+                testDevices={[AdMobBanner.simulatorId]}
+                onAdFailedToLoad={error => console.log(error)}
+            />
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    containerAD: {
+        alignSelf: "stretch",
+        justifyContent: "center",
+        marginHorizontal: normalize(2)  
+    }
+})
+
+export default AdSense;
