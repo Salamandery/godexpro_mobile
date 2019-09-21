619fd6a9cc9ef73b163a75d590b79cd0f371d7c2
salamandery <sp.ront@hotmail.com>
2019-09-21 08:56:45 -0300

    if paid

--- src/components/AdSense/index.js
+++ src/components/AdSense/index.js
@@ -18,22 +18,22 @@ Platform.OS === 'ios'
     : 'ca-app-pub-5356914343536529/6894497090';
 const advert = firebase.admob().interstitial(unitId);
 
-const AdSense = () => {
+const AdSense = ({paid}) => {
     advert.loadAd(request.build());
     advert.on('onAdLoaded', () => {
         console.log('Advert ready to show.');
         advert.show();
     });
     return (
-        <View style={styles.containerAD}>
-            <Banner
+        <View style={paid === true ? styles.containerAD : null}>
+            { paid === true ? (<Banner
                 unitId={unitId}
                 size={'SMART_BANNER'}
                 request={request.build()}
                 onAdLoaded={() => {
                     console.log('Advert loaded');
                 }}
-            />
+            />) : null}
         </View>
     );
 }
